30e9eecb98b437d0f8ee2587468359746a9fe3a0
이병찬 <lbc@ibyeongchan-ui-noteubug.local>
2023-08-01 16:41:25 +0900

    [feat] Spring boot 설정
    - spring.io 설정
    - lombok 설정
    - spring web
    
    [issue] #5

---
+++ BackEnd/FourEver/src/main/java/A4/FourEver/FourEverApplication.java
@@ -0,0 +1,13 @@
+package A4.FourEver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourEverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourEverApplication.class, args);
+	}
+
+}
