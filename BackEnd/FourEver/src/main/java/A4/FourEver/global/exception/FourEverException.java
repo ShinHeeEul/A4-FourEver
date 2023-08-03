3a1f1118b229508f88724fe0b41efdcbba176bac
LBC11 <wpdltm4@gmail.com>
2023-08-03 17:42:41 +0900

    [feat] 전체 custom exception을 관리하기 위한 FourEverException 구현
    [issue] #30

---
+++ BackEnd/FourEver/src/main/java/A4/FourEver/global/exception/FourEverException.java
@@ -0,0 +1,9 @@
+package A4.FourEver.global.exception;
+
+
+public class FourEverException extends RuntimeException {
+
+    public FourEverException(String message) {
+        super(message);
+    }
+}
