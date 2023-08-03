9d63e5aaf3af0c4c0d72b6f0b91c41d1e987e8dd
meanz1 <zndnfjqbd1206@gmail.com>
2023-08-03 14:29:25 +0900

    [feat] 내차만들기/트림 페이지 action 구현 트림 카드 선택 시 Active & userCar 상태 변경 [issue] #8

--- FrontEnd/my-car/src/mycar/Mycar.js
+++ FrontEnd/my-car/src/mycar/Mycar.js
@@ -5,12 +5,15 @@ import SummaryModal from './components/SummaryModal';
 
 function Mycar() {
   const [page, setPage] = useState(0);
-  const [userCar, setUserCar] = useState({});
+  const [userCar, setUserCar] = useState({
+    trim: 'LeBlanc',
+    selectedOptions: [],
+  });
 
   return (
     <>
       <NavBar page={page} />
-      <Outlet setUserCar={setUserCar} />
+      <Outlet context={{ setUserCar }} />
       <SummaryModal userCar={userCar} setPage={setPage} page={page} />
     </>
   );
