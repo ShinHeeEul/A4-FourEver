e39d92586dad21f38792418fc45a0ed3e18e40be
hwangJN <wlsk401@gmail.com>
2023-08-03 10:50:29 +0900

    [feat] 내차만들기 전역상태 관리 내차만들기의 페이지&유저가 선택한 옵션 상태 구축 [issue] #8

--- FrontEnd/my-car/src/mycar/Mycar.js
+++ FrontEnd/my-car/src/mycar/Mycar.js
@@ -1,21 +1,17 @@
 import { Outlet } from 'react-router-dom';
-// import { useState } from 'react';
-// import Trim from './routers/Trim.js';
-// import Engine from './routers/Engine';
-// import BodyType from './routers/BodyType';
-// import WheelDrive from './routers/WheelDrive';
+import { useState } from 'react';
 import NavBar from './components/NavBar';
 import SummaryModal from './components/SummaryModal';
 
 function Mycar() {
-  // const [page, setPage] = useState(0);
-  // const [options, setOptions] = useState({});
+  const [page, setPage] = useState(0);
+  const [userCar, setUserCar] = useState({});
 
   return (
     <>
-      <NavBar />
-      <Outlet />
-      <SummaryModal />
+      <NavBar page={page} />
+      <Outlet setUserCar={setUserCar} />
+      <SummaryModal userCar={userCar} setPage={setPage} page={page} />
     </>
   );
 }
