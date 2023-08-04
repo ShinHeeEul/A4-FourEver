0f697818e46e769b1d86ff8ebfe2df52538951eb
meanz1 <zndnfjqbd1206@gmail.com>
2023-08-04 10:53:52 +0900

    [fix] Engine.js 오류 수정 Engine.js 주석 해제 [issue] #9

--- FrontEnd/my-car/.eslintrc.js
+++ FrontEnd/my-car/.eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  // extends: ['airbnb'],
   extends: ['react-app', 'prettier', 'plugin:prettier/recommended'],
-  // plugins: ['prettier'],
   env: {
     browser: true,
     jest: true,
@@ -39,24 +37,4 @@ module.exports = {
     'react/no-array-index-key': 'off',
     'react/function-component-definition': 2,
   },
-
-  // rules: {
-  //   'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-  //   'react/react-in-jsx-scope': 'on',
-  //   'react/prop-types': 'on',
-  //   'operator-linebreak': ['error', 'after'],
-  //   'no-confusing-arrow': ['error', { allowParens: true }],
-  //   'implicit-arrow-linebreak': 'on',
-  //   'prettier/prettier': 'error',
-  //   eqeqeq: 'off',
-  //   curly: 'error',
-  // },
-  // parser: '@babel/eslint-parser',
-  // parser: '@babel/eslint-parser',
-  // parserOptions: {
-  //   requireConfigFile: false,
-  //   babelOptions: {
-  //     presets: ['@babel/preset-react'],
-  //   },
-  // },
 };
