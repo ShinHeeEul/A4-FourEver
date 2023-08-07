c51e996c98e0ac258ac227ee62b99a6d6a5952fc
meanz1 <zndnfjqbd1206@gmail.com>
2023-08-07 23:18:14 +0900

    [feat] 내차만들기/완성 페이지 배너 뷰 구현 내차만들기 완성페이지 탑 배너 뷰 구현 [issue] #14

--- FrontEnd/my-car/src/mycar/components/CompleteTopBanner.js
+++ FrontEnd/my-car/src/mycar/components/CompleteTopBanner.js
@@ -3,25 +3,52 @@ import palette from '../../style/styleVariable';
 import { CaptionRegular, Heading1Medium } from '../../style/typo';
 import carImg from './image-91.png';
 
-const CarImg = styled.img`
-  z-index: 2;
-`;
 const AllDiv = styled.div`
   display: flex;
   justify-content: center;
-  position: relative;
+  position: fixed;
+  width: 100%;
 `;
 const BannerDiv = styled.div`
   height: 277px;
-  background-color: pink;
-  width: 1280px;
+  width: 100%;
+  margin-top: 30px;
+  background-color: ${palette.LightSand};
+`;
+
+const TextDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
   position: absolute;
+  left: 50%;
+  transform: translate(-90%, 0);
+  margin-top: 60px;
+  margin-left: 50px;
+`;
+const TitleSpan = styled.span`
+  ${Heading1Medium};
+  font-size: 64px;
+  letter-spacing: -1.28px;
+`;
+
+const CompletedMsg = styled.span`
+  ${Heading1Medium};
 `;
 const BgDiv = styled.div`
   display: flex;
   align-items: center;
-  margin-left: 400px;
   position: absolute;
+  left: 50%;
+  transform: translate(-24%, -10%);
+  z-index: 1;
+`;
+
+const CarImg = styled.img``;
+
+const DetailDiv = styled.div``;
+const DetailText = styled.div`
+  ${CaptionRegular}
 `;
 
 function Line() {
@@ -41,10 +68,20 @@ function Line() {
 function CompleteTopBanner() {
   return (
     <AllDiv>
-      <BannerDiv></BannerDiv>
-      <BgDiv>
-        <CarImg src={carImg} />
-      </BgDiv>
+      <BannerDiv>
+        <TextDiv>
+          <TitleSpan>PALISADE</TitleSpan>
+          <Line />
+          <CompletedMsg>나의 펠리세이드가 완성되었어요 !</CompletedMsg>
+          <DetailDiv>
+            <DetailText>완성된 차량은 마이카이빙에서 볼 수 있어요</DetailText>
+            <DetailText>마이카이빙 바로가기</DetailText>
+          </DetailDiv>
+        </TextDiv>
+        <BgDiv>
+          <CarImg src={carImg} />
+        </BgDiv>
+      </BannerDiv>
     </AllDiv>
   );
 }
