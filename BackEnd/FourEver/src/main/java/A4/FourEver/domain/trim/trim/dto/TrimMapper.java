3ead74b8db3ec15c6e7407a256adb4db072d6a84
LBC11 <wpdltm4@gmail.com>
2023-08-10 13:17:01 +0900

    [fix] trim domain Repository, Mapper, Service, Controller 수정
    - Repository: trim_id에 따른 ExColor, ExColorTag 호출 메소드 추가, InColor, InColorTag를 위한 메소드 역시 추가
    - Mapper: 두 Color Set을 아용해 TrimConfigDTO 생성하는 역할
    - Service: Repository, Mapper를 사용해서 Controller에게 TrimConfingDTO 생성 후 전달
    - TrimRepository: 메소드 이름 수정
    
    [issue] #30

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/trim/trim/dto/TrimMapper.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/trim/trim/dto/TrimMapper.java
@@ -1,15 +1,65 @@
 package A4.FourEver.domain.trim.trim.dto;
 
-import A4.FourEver.domain.trim.trim.domain.Trim;
+import A4.FourEver.domain.color.exColor.dto.ExColorInfoDTO;
+import A4.FourEver.domain.color.exColor.dto.ExColorInfoSortedDTO;
+import A4.FourEver.domain.color.inColor.dto.InColorInfoDTO;
+import A4.FourEver.domain.color.inColor.dto.InColorInfoSortedDTO;
+import A4.FourEver.domain.tag.inColorTag.dto.InColorTagInfoDTO;
+import A4.FourEver.domain.tag.exColorTag.dto.ExColorTagInfoDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class TrimMapper {
 
-    public TrimConfigDTO toTrimConfigDTO(final Trim trim) {
+    public TrimConfigDTO toTrimConfigDTO(Set<InColorInfoDTO> inColorInfoDTOs, Set<ExColorInfoDTO> exColorInfoDTOs) {
+        List<InColorInfoSortedDTO> inColorList = inColorInfoDTOs.stream()
+                .sorted(Comparator.comparingLong(InColorInfoDTO::getId))
+                .map(this::convertInColorInfoDTO)
+                .collect(Collectors.toList());
+
+        List<ExColorInfoSortedDTO> exColorList = exColorInfoDTOs.stream()
+                .sorted(Comparator.comparingLong(ExColorInfoDTO::getId))
+                .map(this::convertInColorInfoDTO)
+                .collect(Collectors.toList());
+
+
         return TrimConfigDTO.builder()
-                .inColors(trim.getInColors())
-                .exColors(trim.getExColors())
+                .inColors(inColorList)
+                .exColors(exColorList)
+                .build();
+    }
+
+    private InColorInfoSortedDTO convertInColorInfoDTO(InColorInfoDTO dto) {
+        List<InColorTagInfoDTO> sortedInColorTag = dto.getInColorTagInfoDTOS().stream()
+                .sorted(Comparator.comparingLong(InColorTagInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        return InColorInfoSortedDTO.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .color_image(dto.getColor_image())
+                .in_image(dto.getIn_image())
+                .inColorTagInfoDTOS(sortedInColorTag)
+                .build();
+    }
+
+    private ExColorInfoSortedDTO convertInColorInfoDTO(ExColorInfoDTO dto) {
+        List<ExColorTagInfoDTO> sortedExColorTag = dto.getExColorTagInfoDTOS().stream()
+                .sorted(Comparator.comparingLong(ExColorTagInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        return ExColorInfoSortedDTO.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .color_image(dto.getColor_image())
+                .rotation_image(dto.getRotation_image())
+                .price(dto.getPrice())
+                .exColorTagInfoDTOS(sortedExColorTag)
                 .build();
     }
 }
