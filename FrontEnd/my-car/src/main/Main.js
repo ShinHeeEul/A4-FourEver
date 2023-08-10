a3d6fec93f639f46f1e4abaa6f96302cb857ed88
meanz1 <zndnfjqbd1206@gmail.com>
2023-08-10 16:33:44 +0900

    [feat]main 페이지 구현 메인 페이지 구현 및 전체 페이지 하나로 묶음 [issue] #95

--- FrontEnd/my-car/src/main/Main.js
+++ FrontEnd/my-car/src/main/Main.js
@@ -1,8 +1,30 @@
 import { styled } from 'styled-components';
-const Test = styled.h2`
-  margin-top: 200px;
-`;
+import Btn from './components/EachPageBtn';
+import ToMycar from './components/ToMycar';
+import ToArchiving from './components/ToArchiving';
+import ToMychiving from './components/ToMychiving';
+import MainHeader from './components/MainHeader';
+import { useState, useEffect } from 'react';
+
 function Main() {
-  return <Test>메인페이지입니다</Test>;
+  const [currentDisplay, setCurrentDisplay] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDisplay((prevDisplay) => (prevDisplay % 3) + 1);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <>
+      <MainHeader $DisplayOrder={currentDisplay} />
+      {currentDisplay === 1 && <ToMycar />}
+      {currentDisplay === 2 && <ToArchiving />}
+      {currentDisplay === 3 && <ToMychiving />}
+    </>
+  );
 }
 export default Main;
