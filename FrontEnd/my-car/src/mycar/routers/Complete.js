9658ec5cb268358524e99086d3101fcac3a66628
hwangJN <wlsk401@gmail.com>
2023-08-12 13:04:20 +0900

    [fix] 내차만들기/에러페이지 버그수정 완성 페이지에서 이탈했다가 브라우저 뒤로가기 클릭시 NotFound 컴포넌트 뜨지 않는 문제 해결 [issue] #51

--- FrontEnd/my-car/src/mycar/routers/Complete.js
+++ FrontEnd/my-car/src/mycar/routers/Complete.js
@@ -3,13 +3,15 @@ import CompletedFunction from '../components/completePage/CompletedFunction';
 import AnimationMovingCar from '../components/completePage/AnimationMovingCar';
 import { useEffect } from 'react';
 import NotFound from '../../error/NotFoundPage';
+import { useOutletContext } from 'react-router-dom';
 
 function Complete() {
+  const { userCar } = useOutletContext();
   useEffect(() => {
     localStorage.removeItem('userCar');
   }, []);
 
-  if (!localStorage.getItem('userCar')) {
+  if (!localStorage.getItem('userCar') || !userCar.trim?.id) {
     return <NotFound />;
   }
 
