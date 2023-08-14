72e8eb239f0f6e3436bbb0691b37183f592b7680
hwangJN <wlsk401@gmail.com>
2023-08-14 18:23:58 +0900

    [style] 아카이빙/헤더 삽입 [issue] #146

--- FrontEnd/my-car/src/archiving/RootArchiving.js
+++ FrontEnd/my-car/src/archiving/RootArchiving.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { styled } from 'styled-components';
+import ChivingHeader from '../common/ChivingHeader';
 
 function RootArchiving() {
   return (
     <>
+      <ChivingHeader />
       <Outlet />;
     </>
   );
