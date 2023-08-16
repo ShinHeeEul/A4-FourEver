a421a3b09c7d94dbb65afc004ec95b7a1c09b515
LBC11 <wpdltm4@gmail.com>
2023-08-16 10:36:37 +0900

    [fix] 특정 차 리뷰의 필드 변경
    - car_name 추가
    - is_purchased 추가
    
    [issue] #154

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/review/carReview/dto/CarReviewDetailSortedDTO.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/review/carReview/dto/CarReviewDetailSortedDTO.java
@@ -13,10 +13,12 @@ public class CarReviewDetailSortedDTO {
     private Long car_review_id;
     private Double price;
     private String comment;
+    private String car_name;
     private String trim_name;
     private String engine_name;
     private String drive_name;
     private String body_name;
+    private Integer is_purchased;
     private String exterior_color_name;
     private String interior_color_name;
     private Timestamp created_at;
