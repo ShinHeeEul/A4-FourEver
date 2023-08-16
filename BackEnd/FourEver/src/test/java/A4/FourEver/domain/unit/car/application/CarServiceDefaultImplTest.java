a7461d80f32845be25490a3c8255e039d94c2bfe
LBC11 <wpdltm4@gmail.com>
2023-08-16 17:46:36 +0900

    [test] CarServiceDefaultImplTest 수정
    - CarTrimSortedDTO의 중요한 특성인 정렬되어있는지 여부를 확인하기 위해, 각 리스트별 요소 추가
    
    [issue] #30

--- BackEnd/FourEver/src/test/java/A4/FourEver/domain/unit/car/application/CarServiceDefaultImplTest.java
+++ BackEnd/FourEver/src/test/java/A4/FourEver/domain/unit/car/application/CarServiceDefaultImplTest.java
@@ -15,8 +15,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -39,22 +40,42 @@ class CarServiceDefaultImplTest extends UnitTestBase {
     void getCarTrimsById() {
         // Given
         TrimInfoDTO trimInfo = TrimInfoDTO.builder().id(1L).name("TrimName").image("TrimImage").price(10000.0).build();
+        TrimInfoDTO trimInfo2 = TrimInfoDTO.builder().id(2L).name("TrimName").image("TrimImage").price(10000.0).build();
         BodyInfoDTO bodyInfo = BodyInfoDTO.builder().id(1L).name("BodyName").image("BodyImage").description("BodyDesc").price(20000.0).build();
+        BodyInfoDTO bodyInfo2 = BodyInfoDTO.builder().id(2L).name("BodyName").image("BodyImage").description("BodyDesc").price(20000.0).build();
         DriveInfoDTO driveInfo = DriveInfoDTO.builder().id(1L).name("DriveName").image("DriveImage").description("DriveDesc").price(30000.0).build();
+        DriveInfoDTO driveInfo2 = DriveInfoDTO.builder().id(2L).name("DriveName").image("DriveImage").description("DriveDesc").price(30000.0).build();
         EngineInfoDTO engineInfo = EngineInfoDTO.builder().id(1L).name("EngineName").image("EngineImage").description("EngineDesc").max_output("150hp").max_tok("200Nm").price(40000.0).build();
+        EngineInfoDTO engineInfo2 = EngineInfoDTO.builder().id(2L).name("EngineName").image("EngineImage").description("EngineDesc").max_output("150hp").max_tok("200Nm").price(40000.0).build();
 
         CarTrimsDTO carTrimsDTO = CarTrimsDTO.builder()
-                .trimInfoDTOs(Collections.singleton(trimInfo))
-                .bodyInfoDTOs(Collections.singleton(bodyInfo))
-                .driveInfoDTOs(Collections.singleton(driveInfo))
-                .engineInfoDTOs(Collections.singleton(engineInfo))
+                .trimInfoDTOs(Set.of(trimInfo, trimInfo2))
+                .bodyInfoDTOs(Set.of(bodyInfo, bodyInfo2))
+                .driveInfoDTOs(Set.of(driveInfo, driveInfo2))
+                .engineInfoDTOs(Set.of(engineInfo, engineInfo2))
                 .build();
 
+        List<TrimInfoDTO> trimInfoDTOList = Stream.of(trimInfo, trimInfo2)
+                .sorted(Comparator.comparingLong(TrimInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        List<BodyInfoDTO> bodyInfoDTOList = Stream.of(bodyInfo, bodyInfo2)
+                .sorted(Comparator.comparingLong(BodyInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        List<DriveInfoDTO> driveInfoDTOList = Stream.of(driveInfo, driveInfo2)
+                .sorted(Comparator.comparingLong(DriveInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        List<EngineInfoDTO> engineInfoDTOList = Stream.of(engineInfo, engineInfo2)
+                .sorted(Comparator.comparingLong(EngineInfoDTO::getId))
+                .collect(Collectors.toList());
+
         CarTrimsSortedDTO carTrimsSortedDTO = CarTrimsSortedDTO.builder()
-                .trimInfoDTOs(Arrays.asList(trimInfo))
-                .bodyInfoDTOs(Arrays.asList(bodyInfo))
-                .driveInfoDTOs(Arrays.asList(driveInfo))
-                .engineInfoDTOs(Arrays.asList(engineInfo))
+                .trimInfoDTOs(trimInfoDTOList)
+                .bodyInfoDTOs(bodyInfoDTOList)
+                .driveInfoDTOs(driveInfoDTOList)
+                .engineInfoDTOs(engineInfoDTOList)
                 .build();
 
         given(carRepository.findCarTrimsById(1L)).willReturn(carTrimsDTO);
