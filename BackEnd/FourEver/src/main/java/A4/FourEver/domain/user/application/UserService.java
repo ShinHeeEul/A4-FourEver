48fecb082b94307a0c706ee2079a4720d9f86b66
LBC11 <wpdltm4@gmail.com>
2023-08-19 20:25:20 +0900

    [fix] UserService에 UserFeedDTO를 생성후 반환하는 getUserFeedsById 추가
    [issue] #186

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/user/application/UserService.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/user/application/UserService.java
@@ -1,6 +1,9 @@
 package A4.FourEver.domain.user.application;
 
+import A4.FourEver.domain.user.dto.UserFeedDTO;
+
 public interface UserService {
 
     Long saveUser(String email, String password);
+    UserFeedDTO getUserFeedsById(Long userId);
 }
