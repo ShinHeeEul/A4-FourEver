2326432c7ea18d936310ecc414b5f04cbb167627
LBC11 <wpdltm4@gmail.com>
2023-08-19 13:52:03 +0900

    [fix] MyChiving domain controller 구현
    - userId는 @LoginUserId를 통해 받는다.
    
    [issue] #186

---
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/archiving/api/MyChivingControllerDefaultImpl.java
@@ -0,0 +1,37 @@
+package A4.FourEver.domain.archiving.api;
+
+import A4.FourEver.domain.archiving.application.MyChivingService;
+import A4.FourEver.domain.archiving.dto.MyChivingRequestDTO;
+import A4.FourEver.global.annotation.LoginUserId;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "마이카이빙 정보")
+@RestController
+@RequestMapping("/myChiving")
+public class MyChivingControllerDefaultImpl implements MyChivingController {
+
+    private final MyChivingService myChivingService;
+
+    public MyChivingControllerDefaultImpl(MyChivingService myChivingService) {
+        this.myChivingService = myChivingService;
+    }
+
+    @Override
+    @Operation(summary = "내 차 만들기 완성")
+    @PostMapping("/create")
+    @SecurityRequirement(name = "JWT")
+    public void createMyChiving(@RequestBody final MyChivingRequestDTO dto, @LoginUserId final Long userId) {
+        myChivingService.saveMyChiving(dto, userId);
+    }
+
+    @Override
+    @Operation(summary = "마이카이빙 삭제")
+    @DeleteMapping("/delete/{id}")
+    @SecurityRequirement(name = "JWT")
+    public void deleteMyChiving(@PathVariable final Long id) {
+        myChivingService.removeMyChiving(id);
+    }
+}
