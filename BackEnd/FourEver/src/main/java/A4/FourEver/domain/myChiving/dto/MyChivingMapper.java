ab1242a9254cc076080d23ed596cbcdca18490f0
LBC11 <wpdltm4@gmail.com>
2023-08-19 19:36:37 +0900

    [feat] MyChivingMapper 구현
    - MyChivingDetailDTO를 SortedDTO로 변환
    
    [issue] #186

---
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/myChiving/dto/MyChivingMapper.java
@@ -0,0 +1,55 @@
+package A4.FourEver.domain.myChiving.dto;
+
+import A4.FourEver.domain.option.extraOption.dto.ExtraOptionDetailDTO;
+import A4.FourEver.domain.option.extraOption.dto.ExtraOptionDetailSortedDTO;
+import A4.FourEver.domain.option.extraSubOption.dto.SubExtraOptionNameDTO;
+import A4.FourEver.domain.tag.extraOptionTag.dto.ExtraOptionTagInfoDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class MyChivingMapper {
+
+    public MyChivingDetailSortedDTO convertToSortedDTO(MyChivingDetailDTO dto) {
+        List<ExtraOptionDetailSortedDTO> extraOptionDTOList = dto.getExtraOptionDTOs().stream()
+                .map(this::convertExtraOption)
+                .collect(Collectors.toList());
+
+        return MyChivingDetailSortedDTO.builder()
+                .id(dto.getId())
+                .price(dto.getPrice())
+                .is_end(dto.getIs_end())
+                .car_name(dto.getCar_name())
+                .trimNameDTO(dto.getTrimNameDTO())
+                .engineNameDTO(dto.getEngineNameDTO())
+                .bodyNameDTO(dto.getBodyNameDTO())
+                .driveNameDTO(dto.getDriveNameDTO())
+                .exColorDTO(dto.getExColorDTO())
+                .inColorDTO(dto.getInColorDTO())
+                .extraOptionDTOs(extraOptionDTOList)
+                .build();
+    }
+
+    private ExtraOptionDetailSortedDTO convertExtraOption(ExtraOptionDetailDTO dto) {
+        List<ExtraOptionTagInfoDTO> tagInfoDTOList = dto.getExtraOptionTagInfoDTOS().stream()
+                .sorted(Comparator.comparingLong(ExtraOptionTagInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        List<SubExtraOptionNameDTO> nameDTOList = dto.getSubExtraOptionNameDTOs().stream()
+                .sorted(Comparator.comparingLong(SubExtraOptionNameDTO::getId))
+                .collect(Collectors.toList());
+
+        return ExtraOptionDetailSortedDTO.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .image(dto.getImage())
+                .x_position(dto.getX_position())
+                .y_position(dto.getY_position())
+                .extraOptionTagInfoDTOS(tagInfoDTOList)
+                .subExtraOptionNameDTOs(nameDTOList)
+                .build();
+    }
+}
