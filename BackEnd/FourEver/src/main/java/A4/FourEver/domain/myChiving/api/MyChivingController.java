c7486c387baf33460a3f9e8f6e32f839985e93fa
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-08-20 04:04:18 +0900

    [feat] MyChivingController
    - 내 차 만들기 완성에 설명 추가
     + default 상태가 -1보다 0이 더 적합하다 판단하여 0으로 수정
    - api 이름 변경 : uri에 대문자는 오류를 일으킬 수 있으므로 mychiving으로 변경
    - 특정 마이카이빙 조회 API 추가
    
    [ISSUE] #215

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/myChiving/api/MyChivingController.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/myChiving/api/MyChivingController.java
@@ -1,8 +1,10 @@
 package A4.FourEver.domain.myChiving.api;
 
+import A4.FourEver.domain.myChiving.dto.MyChivingDetailSortedDTO;
 import A4.FourEver.domain.myChiving.dto.MyChivingSaveDTO;
 
 public interface MyChivingController {
     void createMyChiving(final MyChivingSaveDTO dto, final Long userId);
+    MyChivingDetailSortedDTO getMyChivingDetail(final Long id);
     void deleteMyChiving(final Long id);
 }
