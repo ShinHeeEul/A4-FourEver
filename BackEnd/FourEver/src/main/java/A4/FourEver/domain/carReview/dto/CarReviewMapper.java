f5675a64acba18e6208dc45c404e64eab969318c
LBC11 <wpdltm4@gmail.com>
2023-08-21 12:50:44 +0900

    [fix] CarReviewMapper에 CarReviewResultDTO에서 CarReviewResultSortedDTO로 변환하는 메소드 추가
    [issue] #186

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/carReview/dto/CarReviewMapper.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/carReview/dto/CarReviewMapper.java
@@ -1,9 +1,19 @@
 package A4.FourEver.domain.carReview.dto;
 
+import A4.FourEver.domain.color.exColor.dto.ExColorInfoDTO;
+import A4.FourEver.domain.color.exColor.dto.ExColorInfoSortedDTO;
+import A4.FourEver.domain.color.inColor.dto.InColorInfoDTO;
+import A4.FourEver.domain.color.inColor.dto.InColorInfoSortedDTO;
+import A4.FourEver.domain.option.defaultOption.dto.DefaultOptionInfoDTO;
 import A4.FourEver.domain.option.extraOption.dto.ExtraOptionDetailDTO;
 import A4.FourEver.domain.option.extraOption.dto.ExtraOptionDetailSortedDTO;
+import A4.FourEver.domain.option.extraOption.dto.ExtraOptionInfoDTO;
+import A4.FourEver.domain.option.extraOption.dto.ExtraOptionInfoSortedDTO;
+import A4.FourEver.domain.option.extraSubOption.dto.SubExtraOptionInfoDTO;
 import A4.FourEver.domain.option.extraSubOption.dto.SubExtraOptionNameDTO;
+import A4.FourEver.domain.tag.exColorTag.dto.ExColorTagInfoDTO;
 import A4.FourEver.domain.tag.extraOptionTag.dto.ExtraOptionTagInfoDTO;
+import A4.FourEver.domain.tag.inColorTag.dto.InColorTagInfoDTO;
 import A4.FourEver.domain.tag.totalTag.dto.TotalTagInfoDTO;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +24,7 @@ import java.util.stream.Collectors;
 @Component
 public class CarReviewMapper {
 
-    public CarReviewDetailSortedDTO convertToSortedDTO(CarReviewDetailDTO dto) {
+    public CarReviewDetailSortedDTO convertToSortedDTO(final CarReviewDetailDTO dto) {
         List<ExtraOptionDetailSortedDTO> extraOptionDTOList = dto.getExtraOptionDetailDTOS().stream()
                 .map(this::convertExtraOption)
                 .collect(Collectors.toList());
@@ -41,6 +51,58 @@ public class CarReviewMapper {
                 .build();
     }
 
+    public CarReviewResultSortedDTO convertToSortedDTO(final CarReviewResultDTO dto, final String car_name) {
+        List<ExtraOptionInfoSortedDTO> extraOptionList = dto.getExtraOptionDTOs().stream()
+                .sorted(Comparator.comparingLong(ExtraOptionInfoDTO::getId))
+                .map(this::convertExtraOptionInfoDTO)
+                .collect(Collectors.toList());
+
+        List<DefaultOptionInfoDTO> defaultOptionList = dto.getDefaultOptionDTOs().stream()
+                .sorted(Comparator.comparingLong(DefaultOptionInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        InColorInfoDTO inColorInfoDTO = dto.getInColorDTO();
+
+        List<InColorTagInfoDTO> inColorTagList = inColorInfoDTO.getInColorTagInfoDTOS().stream()
+                .sorted(Comparator.comparingLong(InColorTagInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        InColorInfoSortedDTO inColorInfoSortedDTO = InColorInfoSortedDTO.builder()
+                .id(inColorInfoDTO.getId())
+                .name(inColorInfoDTO.getName())
+                .color_image(inColorInfoDTO.getColor_image())
+                .in_image(inColorInfoDTO.getIn_image())
+                .inColorTagInfoDTOS(inColorTagList)
+                .build();
+
+        ExColorInfoDTO exColorInfoDTO = dto.getExColorDTO();
+
+        List<ExColorTagInfoDTO> exColorTagList = exColorInfoDTO.getExColorTagInfoDTOS().stream()
+                .sorted(Comparator.comparingLong(ExColorTagInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        ExColorInfoSortedDTO exColorInfoSortedDTO = ExColorInfoSortedDTO.builder()
+                .id(exColorInfoDTO.getId())
+                .name(exColorInfoDTO.getName())
+                .color_image(exColorInfoDTO.getColor_image())
+                .rotation_image(exColorInfoDTO.getRotation_image())
+                .price(exColorInfoDTO.getPrice())
+                .exColorTagInfoDTOS(exColorTagList)
+                .build();
+
+        return CarReviewResultSortedDTO.builder()
+                .car_name(car_name)
+                .trimInfoDTO(dto.getTrimInfoDTO())
+                .engineInfoDTO(dto.getEngineInfoDTO())
+                .bodyInfoDTO(dto.getBodyInfoDTO())
+                .driveInfoDTO(dto.getDriveInfoDTO())
+                .inColorDTO(inColorInfoSortedDTO)
+                .exColorDTO(exColorInfoSortedDTO)
+                .defaultOptionDTOs(defaultOptionList)
+                .extraOptionDTOs(extraOptionList)
+                .build();
+    }
+
     private ExtraOptionDetailSortedDTO convertExtraOption(ExtraOptionDetailDTO dto) {
         List<ExtraOptionTagInfoDTO> tagInfoDTOList = dto.getExtraOptionTagInfoDTOS().stream()
                 .sorted(Comparator.comparingLong(ExtraOptionTagInfoDTO::getId))
@@ -58,6 +120,29 @@ public class CarReviewMapper {
                 .y_position(dto.getY_position())
                 .extraOptionTagInfoDTOS(tagInfoDTOList)
                 .subExtraOptionNameDTOs(nameDTOList)
+                .build();
+    }
+
+    private ExtraOptionInfoSortedDTO convertExtraOptionInfoDTO(final ExtraOptionInfoDTO dto) {
+        List<ExtraOptionTagInfoDTO> sortedOptionTags = dto.getExtraOptionTagInfoDTOS().stream()
+                .sorted(Comparator.comparingLong(ExtraOptionTagInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        List<SubExtraOptionInfoDTO> sortedSubOptions = dto.getSubExtraOptionInfoDTOs().stream()
+                .sorted(Comparator.comparingLong(SubExtraOptionInfoDTO::getId))
+                .collect(Collectors.toList());
+
+        return ExtraOptionInfoSortedDTO.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .category(dto.getCategory())
+                .image(dto.getImage())
+                .price(dto.getPrice())
+                .x_position(dto.getX_position())
+                .y_position(dto.getY_position())
+                .extraOptionTagInfoDTOS(sortedOptionTags)
+                .subExtraOptionInfoDTOs(sortedSubOptions)
                 .build();
     }
 }
