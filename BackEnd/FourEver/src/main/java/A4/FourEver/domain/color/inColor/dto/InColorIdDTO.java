4e9e747522cb84c611023db7cba396a6f9f212b1
LBC11 <wpdltm4@gmail.com>
2023-08-21 12:47:26 +0900

    [feat] ExColor, InColor의 id를 담은 DTO 구현
    - ExColorIdDTO
    - InColorIdDTO
    
    [issue] #186

---
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/color/inColor/dto/InColorIdDTO.java
@@ -0,0 +1,12 @@
+package A4.FourEver.domain.color.inColor.dto;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Builder
+@Getter
+@EqualsAndHashCode
+public class InColorIdDTO {
+    private Long id;
+}
