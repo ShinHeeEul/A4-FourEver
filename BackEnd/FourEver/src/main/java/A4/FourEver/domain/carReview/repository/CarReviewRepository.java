6e870a3214ed5176993c8e7a13cb67aacc1b3703
LBC11 <wpdltm4@gmail.com>
2023-08-21 13:48:16 +0900

    [feat] CarReviewRepository 수정
    -findCarReviewResult: 기존 데이터를 이용한 내 차 만들기 API
    
    [issue] #186

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/carReview/repository/CarReviewRepository.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/carReview/repository/CarReviewRepository.java
@@ -1,8 +1,10 @@
 package A4.FourEver.domain.carReview.repository;
 
 import A4.FourEver.domain.carReview.dto.CarReviewDetailDTO;
+import A4.FourEver.domain.carReview.dto.CarReviewIdDTO;
+import A4.FourEver.domain.carReview.dto.CarReviewResultDTO;
 
 public interface CarReviewRepository {
     CarReviewDetailDTO findCarReviewDetail(final Long id);
-
+    CarReviewResultDTO findCarReviewResult(final CarReviewIdDTO carReviewIdDTO);
 }
