e6c3aa5f4814c1939f88319d81b7e541971ae7b2
LBC11 <wpdltm4@gmail.com>
2023-08-22 21:59:41 +0900

    [fix] ModelServiceDefaultImpl에서 기본 품목 목록과 선택 품목 목록 모두 비어있을 경우 OptionsNotFoundException 발생하도록 수정
    [issue] #248

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/model/application/ModelServiceDefaultImpl.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/model/application/ModelServiceDefaultImpl.java
@@ -1,6 +1,7 @@
 package A4.FourEver.domain.model.application;
 
 import A4.FourEver.domain.model.dto.*;
+import A4.FourEver.domain.model.exception.OptionsNotFoundException;
 import A4.FourEver.domain.model.repository.ModelRepository;
 import A4.FourEver.domain.option.defaultOption.dto.DefaultOptionInfoDTO;
 import A4.FourEver.domain.option.extraOption.dto.ExtraOptionInfoDTO;
@@ -23,6 +24,7 @@ public class ModelServiceDefaultImpl implements ModelService {
     public ModelOptionsSortedDTO getModelOptions(final Long trim_id, final Long engine_id, final Long body_id, final Long drive_id) {
         Set<DefaultOptionInfoDTO> defaultOptionInfoDTOs = modelRepository.findModelDefaultOption(trim_id, engine_id, body_id, drive_id);
         Set<ExtraOptionInfoDTO> extraOptionInfoDTOs = modelRepository.findModelExtraOption(trim_id, engine_id, body_id, drive_id);
+        if(defaultOptionInfoDTOs.isEmpty() && extraOptionInfoDTOs.isEmpty()) throw new OptionsNotFoundException(trim_id, engine_id, body_id, drive_id);
         return modelMapper.convertToSortedDTO(extraOptionInfoDTOs, defaultOptionInfoDTOs);
     }
 }
