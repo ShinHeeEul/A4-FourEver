d3c4ae0bb2981111e1d841ad010e22d4827c0466
LBC11 <wpdltm4@gmail.com>
2023-08-22 19:56:09 +0900

    [fix] CarReviewIdDTO의 필드에 제약 추가
    [issue] #248

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/carReview/dto/CarReviewIdDTO.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/carReview/dto/CarReviewIdDTO.java
@@ -2,18 +2,43 @@ package A4.FourEver.domain.carReview.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @Getter
 public class CarReviewIdDTO {
+
+    @NotBlank(message = "차 이름은 공란일 수 없습니다.")
+    @NotNull(message = "차 이름이 없습니다.")
     private String car_name;
+
+    @NotNull(message = "트림 ID가 없습니다.")
+    @Positive(message = "트림 ID는 양수여야 합니다.")
     private Long trim_id;
+
+    @NotNull(message = "엔진 ID가 없습니다.")
+    @Positive(message = "엔진 ID는 양수여야 합니다.")
     private Long engine_id;
+
+    @NotNull(message = "바디타입 ID가 없습니다.")
+    @Positive(message = "바디타입 ID는 양수여야 합니다.")
     private Long body_id;
+
+    @NotNull(message = "구동방식 ID가 없습니다.")
+    @Positive(message = "구동방식 ID는 양수여야 합니다.")
     private Long drive_id;
+
+    @NotNull(message = "내부 색상 ID가 없습니다.")
+    @Positive(message = "내부 색상 ID는 양수여야 합니다.")
     private Long in_color_id;
+
+    @NotNull(message = "외부 색상 ID가 없습니다.")
+    @Positive(message = "외부 색상 ID는 양수여야 합니다.")
     private Long ex_color_id;
-    private List<Long> extra_option_ids;
+
+    private List<@NotNull @Positive(message = "추가 옵션 ID는 양수여야 합니다.") Long> extra_option_ids;
 
     public CarReviewIdDTO() {
     }
