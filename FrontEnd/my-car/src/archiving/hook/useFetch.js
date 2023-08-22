c5bcbb4bdb0c0d6ac4f4c63711270bcd043bf58c
hwangJN <wlsk401@gmail.com>
2023-08-22 22:21:21 +0900

    [feat] 아카이빙 데이터 만료시간 설정 로컬스토리지에 저장되어있는 아카이빙 데이터의 만료시간 필드를 추가하여 만료되었을 경우 일단 기존 캐시 데이터를 보여준 다음 패치를 진행햐여 자연스럽게 리렌더링 [issue] #270

--- FrontEnd/my-car/src/archiving/hook/useFetch.js
+++ FrontEnd/my-car/src/archiving/hook/useFetch.js
@@ -1,39 +1,62 @@
 import { useEffect, useState } from 'react';
+import { MakePath } from '../../api';
+import { ARCHIVING } from '../../constant';
 
 const useFetch = ({ url, config, optionSelect, activeTab }) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
-
-  const fetchData = async () => {
-    // console.log(url);
-    try {
-      const response = await fetch(url, { ...config });
-      const jsonData = await response.json();
-
-      if (config?.body) {
-        const body = JSON.parse(config.body);
-        const entire = { ...body, activeTab };
-        localStorage.setItem(JSON.stringify(entire), JSON.stringify(jsonData));
-      }
-      setData(jsonData);
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-      setLoading(false);
-    }
-  };
+  const [controller, setController] = useState();
 
   useEffect(() => {
+    const fetchData = async () => {
+      // setController(new AbortController());
+      try {
+        const response = await fetch(url, {
+          ...config,
+          // signal: controller.signal,
+        });
+        const jsonData = await response.json();
+
+        if (config?.body) {
+          const body = JSON.parse(config.body);
+          const now = new Date();
+          const expirationMinutes = 0.1;
+          const expiration = now.getTime() + expirationMinutes * 60 * 1000;
+          const entire = { ...body, activeTab };
+          localStorage.setItem(
+            JSON.stringify(entire),
+            JSON.stringify({ data: jsonData, expiration }),
+          );
+        }
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+        setLoading(false);
+      }
+    };
+
     const entire = { ...optionSelect, activeTab };
     const localData = localStorage.getItem(JSON.stringify(entire));
+
     if (localData) {
-      setData(JSON.parse(localData));
+      setData(JSON.parse(localData).data);
+
+      const data = JSON.parse(localData);
+      const now = new Date().getTime();
+      if (now > data.expiration) {
+        // controller?.abort();
+        fetchData();
+      }
+
       setLoading(false);
     } else {
       setLoading(true);
       fetchData();
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [optionSelect, activeTab]);
 
