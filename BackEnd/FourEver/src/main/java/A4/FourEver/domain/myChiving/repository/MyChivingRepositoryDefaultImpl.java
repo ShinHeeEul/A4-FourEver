5a350ca5f5cb9381ab8afa5dfd6a6bca7bdcdcce
LBC11 <wpdltm4@gmail.com>
2023-08-22 19:40:23 +0900

    [fix] MyChivingRepositoryDefaultImpl에서 price의 기본값 설정 수정
    - price가 0일 경우 41980000으로 설정해주었는데, 해당 사항은 기본값을 사용했을 때의 상황이므로 다시 0으로 설정
    
    [issue] #248

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/myChiving/repository/MyChivingRepositoryDefaultImpl.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/myChiving/repository/MyChivingRepositoryDefaultImpl.java
@@ -51,7 +51,7 @@ public class MyChivingRepositoryDefaultImpl implements MyChivingRepository {
 
         MapSqlParameterSource params = new MapSqlParameterSource();
         params.addValue("is_end", dto.getIs_end());
-        params.addValue("price", dto.getPrice() == 0 ? 41980000 : dto.getPrice());
+        params.addValue("price", dto.getPrice());
         params.addValue("user_id", userId);
         params.addValue("ex_color_id", getOrDefault(dto.getEx_color_id()));
         params.addValue("in_color_id", getOrDefault(dto.getIn_color_id()));
