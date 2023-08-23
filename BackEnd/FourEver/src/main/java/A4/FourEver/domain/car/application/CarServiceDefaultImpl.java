dc1a2c680d4265390fd54cddfc61b6d1102b1cc2
LBC11 <wpdltm4@gmail.com>
2023-08-23 12:04:25 +0900

    [fix] CarReviewResultDTO의 구매 혹은 시승 후기 리스트가 비어있을 경우 CarReviewListNotFoundException 발생하도록 수정
    [issue] #248

--- BackEnd/FourEver/src/main/java/A4/FourEver/domain/car/application/CarServiceDefaultImpl.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/domain/car/application/CarServiceDefaultImpl.java
@@ -1,6 +1,7 @@
 package A4.FourEver.domain.car.application;
 
 import A4.FourEver.domain.car.dto.*;
+import A4.FourEver.domain.car.exception.CarReviewListNotFoundException;
 import A4.FourEver.domain.car.repository.CarRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,17 +24,21 @@ public class CarServiceDefaultImpl implements CarService {
     }
 
     @Override
-    public CarExtraOptionNameDTO getCarExtraOptionNameById(Long id) {
+    public CarExtraOptionNameDTO getCarExtraOptionNameById(final Long id) {
         return carRepository.findCarExtraOptionNameById(id);
     }
 
     @Override
     public CarReviewOverviewSortedListDTO getAllCarReviewOverviewList(final Long id, final List<Long> extraOptionIds) {
-        return carMapper.convertToSortedDTO(carRepository.findAllCarReviewOverviewList(id, extraOptionIds));
+        CarReviewOverviewListDTO carReviewList = carRepository.findAllCarReviewOverviewList(id, extraOptionIds);
+        if(carReviewList.getCarReviewOverviewDTOs().isEmpty()) throw new CarReviewListNotFoundException(id);
+        return carMapper.convertToSortedDTO(carReviewList);
     }
 
     @Override
     public CarReviewOverviewSortedListDTO getPartialCarReviewOverviewList(final Long id, final Integer isPurchased, final List<Long> extraOptionIds) {
-        return carMapper.convertToSortedDTO(carRepository.findPartialCarReviewOverviewList(id, isPurchased, extraOptionIds));
+        CarReviewOverviewListDTO carReviewPartialList = carRepository.findPartialCarReviewOverviewList(id, isPurchased, extraOptionIds);
+        if(carReviewPartialList.getCarReviewOverviewDTOs().isEmpty()) throw new CarReviewListNotFoundException(id);
+        return carMapper.convertToSortedDTO(carReviewPartialList);
     }
 }
