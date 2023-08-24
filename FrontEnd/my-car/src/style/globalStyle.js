36d490076c5357367a44a12b3091b9a983a4c38e
meanz1 <zndnfjqbd1206@gmail.com>
2023-08-24 10:36:34 +0900

    [style] 폰트 수정

--- FrontEnd/my-car/src/style/globalStyle.js
+++ FrontEnd/my-car/src/style/globalStyle.js
@@ -3,12 +3,12 @@ import palette from './styleVariable';
 
 const GlobalStyle = createGlobalStyle`
 
-  @font-face {
+  /* @font-face {
     font-family: 'Hyundai Sans Head KR';
     src: url('./fonts/HyundaiSans.woff2') format('font-woff2');
     font-weight: normal;
     font-style: normal;
-  }
+  } */
   
   html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
@@ -27,14 +27,14 @@ time, mark, audio, video,input, textarea,select,option {
   padding: 0;
   border: 0;
   font-size: 100%;
-  /* font: inherit; */
-  font-family: 'Hyundai Sans Head KR', sans-serif;
+  font: inherit;
+  /* font-family: 'Hyundai Sans Head KR', sans-serif; */
   vertical-align: baseline;
   scroll-behavior: smooth;
 }
 body{
-  font-family: 'Hyundai Sans Head KR';
-
+  /* font-family: 'Hyundai Sans Head KR'; */
+/*  */
 }
 body.no-scroll {
   height: 100%;
