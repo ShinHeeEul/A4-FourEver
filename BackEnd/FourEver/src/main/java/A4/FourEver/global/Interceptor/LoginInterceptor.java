16d1afefbb18dcfeba25cde77c4930bb35d77d99
HeeEul Shin <83682424+ShinHeeEul@users.noreply.github.com>
2023-08-27 22:37:55 +0900

    [BE] CORS 개선 (#295)
    * [feat] CarReviewOnBoardingDTO .png를 .webp로 변경
    
    [ISSUE] #294
    
    * [feat] WebMvcConfig .allowedOrigin 범위 제한
    
    [ISSUE] #294
    
    * [feat] AuthConfig API에 로그인 없이 요청하면 에러 나도록 변경
    
    [ISSUE] #294
    
    * [feat] LoginInterceptor Login 이전에 CORS를 하도록 설정
    
    [ISSUE] #294

--- BackEnd/FourEver/src/main/java/A4/FourEver/global/Interceptor/LoginInterceptor.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/global/Interceptor/LoginInterceptor.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
@@ -18,6 +20,28 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+
+        String origin = request.getHeader("Origin");
+        List<String> allowedOrigins = Arrays.asList("http://localhost:80",
+                "http://www.hyundaimycar.store",
+                "https://www.hyundaimycar.store",
+                "http://www.hyundaimycar.store/",
+                "https://www.hyundaimycar.store:443",
+                "https://www.hyundaimycar.store/",
+                "http://www.hyundaimycar.store:80");
+
+        if (allowedOrigins.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization");
+
+        if(request.getMethod().equals("OPTIONS")) {
+            response.setStatus(200);
+            return true;
+        }
+
         if (isSwaggerRequest(request)) {
             return true;
         }
--- BackEnd/FourEver/src/main/java/A4/FourEver/global/config/WebMvcConfig.java
+++ BackEnd/FourEver/src/main/java/A4/FourEver/global/config/WebMvcConfig.java
@@ -10,8 +10,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins("http://localhost:80",
+                        "http://www.hyundaimycar.store",
+                        "https://www.hyundaimycar.store",
+                        "http://www.hyundaimycar.store/",
+                        "https://www.hyundaimycar.store:443",
+                        "https://www.hyundaimycar.store/",
+                        "http://www.hyundaimycar.store:80")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization");
     }
 }
